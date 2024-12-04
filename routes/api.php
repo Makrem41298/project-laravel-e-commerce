5e7d48a33c6b214c64c72c59d48f2577419cb1a4
Samah <samahbenabdallah793@gmail.com>
2024-12-04 14:06:39 +0100

    message

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\roleController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +22,10 @@ Route::post('categories',[CategorieController::class,'store']);
 Route::put('categories/{id}',[CategorieController::class,'update']);
 Route::delete('categories/{id}',[CategorieController::class,'destroy']);
 Route::get('categories/{id}',[CategorieController::class,'show']);
+
+
+Route::get('role',[roleController::class,'index']);
+Route::post('role',[roleController::class,'store']);
+Route::put('role/{id}',[roleController::class,'update']);
+Route::delete('role/{id}',[roleController::class,'destroy']);
+Route::get('role/{id}',[roleController::class,'show']);
