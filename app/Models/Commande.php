9a08dd311d0278c0b9cf32dc50bf7e2b7fd06160
makrem41298 <makrem050@live.fr>
2024-12-04 22:43:10 +0100

    relationship Command and produit

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Commande extends Model
 {
     use HasFactory;
+    public function produits(): BelongsToMany
+    {
+        return $this->belongsToMany(Produit::class,'passe_commandes');
+    }
 }
