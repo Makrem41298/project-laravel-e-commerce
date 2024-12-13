f4ac90705007673fdb9383716f2102bacc9097a7
makrem41298 <makrem050@live.fr>
2024-12-13 22:18:35 +0100

    model finis

--- database/migrations/2024_12_13_202658_create_paiments_table.php
+++ database/migrations/2024_12_13_202658_create_paiments_table.php
@@ -13,9 +13,16 @@ return new class extends Migration
     {
         Schema::create('paiments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('commande_id')->constrained('commandes');
+            $table->enum('payment_method', ['carte_credit', 'paypal', 'virement_bancaire', 'especes'])->default('carte_credit'); // Enum pour les méthodes de paiement
+            $table->decimal('amount', 10, 2); // Montant avec deux décimales
+            $table->enum('status', ['en_attente', 'termine', 'echoue'])->default('en_attente'); // Enum pour le statut
+            $table->timestamp('paid_at')->nullable(); // Date de finalisation du paiement
             $table->timestamps();
+
         });
     }
+
 
     /**
      * Reverse the migrations.
